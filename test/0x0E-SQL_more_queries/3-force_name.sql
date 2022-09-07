4fd0a016173d865d7d9f749d39730841b53ec674
okoye victor <okoyeebuka64@gmail.com>
2022-09-07 15:22:49 -0700

    new

--- test/0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,8 +0,0 @@
--- Script that creates the MySQL server user user_0d_1.
-
--- Create table
-CREATE TABLE IF NOT EXISTS `force_name` (
-  `id` INT, 
-  -- NOT NULL, Constrain column value to be not NULL --
-  `name` VARCHAR(256) NOT NULL
-);
